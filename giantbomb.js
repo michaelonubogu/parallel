de4a7bfc40c0ece81948a20ae4337314b9ee31d6
Michael Onubogu <nonubogu@gmail.com>
2015-08-19 12:20:40 -0400

    te

--- giantbomb.js
+++ giantbomb.js
@@ -12,26 +12,28 @@ var giantbomb = {
         var gbRef = config.giant_bomb;
         var limit = max !== null && max !== undefined ? max : gbRef.limit;
         var gburl = gbRef.url + gbRef.endpoints.search + '/?api_key=' + gbRef.key + '&limit=' + limit + '&format=' + gbRef.formats.json + '&query="' + query + '"&resources=' + gbRef.resources.game + '&field_list=id,name,image,original_release_date';
-        
-        if (config.appsettings.env == 'dev') {
-            return rp(gburl);
-        }
-        else {
-            if (process.env.PROXIMO_URL) {
-                var proxy = url.parse(process.env.PROXIMO_URL);
-                options = {
-                    uri: gburl,
-                    headers: { "Proxy-Authorization" : 'Basic #{new Buffer(proxy.auth).toString("base64")}' }
-                };
+        return rp(gburl);
+        //if (config.appsettings.env == 'dev') {
+        //    return rp(gburl);
+        //}
+        //else {
+        //    if (process.env.PROXIMO_URL) {
+        //        var proxy = url.parse(process.env.PROXIMO_URL);
+        //        options = {
+        //            hostname: proxy.hostname,
+        //            port: proxy.port || 80,
+        //            uri: gburl,
+        //            headers: { "Proxy-Authorization" : 'Basic #{new Buffer(proxy.auth).toString("base64")}' }
+        //        };
                 
-                return rp(options);
+        //        return rp(options);
 
-                //return http.get(options, function (res) {
-                //    console.log("status code", res.statusCode);
-                //    console.log("headers", res.headers);
-                //});
-            }
-        }
+        //        //return http.get(options, function (res) {
+        //        //    console.log("status code", res.statusCode);
+        //        //    console.log("headers", res.headers);
+        //        //});
+        //    }
+        //}
     },
 
     getGame: function(){
