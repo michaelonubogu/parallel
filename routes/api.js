dcdb30fd196eb74820181c8505e36c11d4013e0a
Michael Onubogu <nonubogu@gmail.com>
2015-08-19 07:29:45 -0400

    fayed - error fix

--- routes/api.js
+++ routes/api.js
@@ -40,13 +40,7 @@ var server = http.createServer();
 var faye_server = new faye.NodeAdapter({ mount: '/faye', timeout: 45 });
 console.log('Firing up faye server. . . ');
 faye_server.attach(server);
-
-if (config.appsettings.env == 'dev') {
-    server.listen(8089);
-}
-else {
-    server.listen(8089, host);
-}
+server.listen(8089);
 
 var relyingParty = new openid.RelyingParty(
 							origin + '/api/steam/authenticate/verify', // Verification URL (yours)
