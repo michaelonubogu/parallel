e8df713351fff6f939af96923d7bd0906155c1bf
Michael Onubogu <nonubogu@gmail.com>
2015-08-19 11:50:53 -0400

    TE

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
 	app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        console.log(err.message);
+        console.log('ERROR HERE: ' + err.message);
 		//res.render('error', {
 		//	message: err.message,
 		//	error: err
@@ -68,7 +68,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    console.log(err.message);
+    console.log('ERROR HERE:' + err.message);
 	//res.render('error', {
 	//	message: err.message,
 	//	error: {}
